a5819239d57ff1d4d8da74baffe937f560581d26
Joshtri <stuffofyos1516@gmail.com>
2024-07-15 15:32:48 +0800

    fix

--- routes/post.route.js
+++ routes/post.route.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get('/post', postController.getPost);
 router.get('/post/:id', postController.getPostById);
 
+router.get('/test', (req,res)=>{
+    res.render('test anjing');
+});
 
 export default router;
