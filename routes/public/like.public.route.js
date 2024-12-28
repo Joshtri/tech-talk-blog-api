1d0646b2415a9429cc7d710f99f95858f7bb1cce
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 13:44:11 +0800

    adding public route

---
+++ routes/public/like.public.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+// import * as  from "../../controllers/like/index.js"; // Mengimpor controller dari index.js
+import { likePublicController } from "../../controllers/like/index.js";
+
+const likePublicRoute = express.Router();
+
+
+
+// Route untuk menyukai postingan
+likePublicRoute.post('/like', likePublicController.likePost);
+
+// Route untuk membatalkan suka pada postingan
+likePublicRoute.post('/unlike', likePublicController.unlikePost);
+
+// Route untuk mendapatkan jumlah like pada postingan
+likePublicRoute.get('/like/:postId', likePublicController.getLikeCountByPostId);
+
+export default likePublicRoute;
