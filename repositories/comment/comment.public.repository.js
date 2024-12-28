07668057d78eedb54213fffd6d7922408770549a
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 11:14:26 +0800

    separate api, focussing by domain comment

---
+++ repositories/comment/comment.public.repository.js
@@ -0,0 +1,21 @@
+// repositories/comment.repository.js
+import Comment from "../../models/comment.model.js";
+
+const findCommentsByPostId = async (postId) => {
+    return await Comment.find({ postId });
+};
+
+const createComment = async (commentData) => {
+    const comment = new Comment(commentData);
+    return await comment.save();
+};
+
+const countCommentsByPostId = async (postId) => {
+    return await Comment.countDocuments({ postId });
+};
+
+export default {
+    findCommentsByPostId,
+    createComment,
+    countCommentsByPostId
+}
