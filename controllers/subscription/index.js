110341fc1e05579f0621a767309dda6d4a23b8b7
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 13:42:07 +0800

    merge private and public subscroptopm controller  in index.js

---
+++ controllers/subscription/index.js
@@ -0,0 +1,4 @@
+import subscriptionCmsController from "./subscription.cms.controller.js";
+import subscriptionPublicController from "./subscription.public.controller.js";
+
+export { subscriptionPublicController,subscriptionCmsController };
