dd013ce78fc332a8e0ad149d4d0fbac3ece5c5ae
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 21:19:45 +0800

    new file name

---
+++ repositories/subscription/subscription.private.repository.js
@@ -0,0 +1,28 @@
+import Subscription from "../../models/subscription.model.js";
+
+
+const getTotalSubscription = async () => {
+    try {
+        const totalSubscription = await Subscription.countDocuments();
+        return totalSubscription;
+    } catch (error) {
+        console.log(`error get total subscription [REPO] : ${error}`);
+
+        throw error;
+    }
+};
+
+const getAllSubscription = async()=>{
+    try {
+        const subscriptions = await Subscription.find();
+        return subscriptions;
+    } catch (error) {
+        console.log(`error get all subscription [REPO] : ${error}`);
+        throw error;
+    }
+};
+
+export default {
+    getAllSubscription,
+    getTotalSubscription
+};
