665f0117f6bc484a860ff8f7cf2f15ac04f9f660
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 21:24:21 +0800

    adjusment new file name

--- controllers/subscription/index.js
+++ controllers/subscription/index.js
@@ -1,4 +1,4 @@
-import subscriptionCmsController from "./subscription.cms.controller.js";
+import subscriptionPrivateController from "./subscription.private.controller.js";
 import subscriptionPublicController from "./subscription.public.controller.js";
 
-export { subscriptionPublicController,subscriptionCmsController };
+export { subscriptionPublicController,subscriptionPrivateController };
