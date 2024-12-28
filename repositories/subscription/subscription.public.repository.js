67f51ef99ce2b1d8acfc36156d4864027308cbc6
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 11:38:48 +0800

    separate api, focussing by domain subscription

---
+++ repositories/subscription/subscription.public.repository.js
@@ -0,0 +1,12 @@
+// repositories/subscription.repository.js
+import Subscription from "../../models/subscription.model.js";
+
+const createSubscription = async (subscriptionData) => {
+    const subscription = new Subscription(subscriptionData);
+    return await subscription.save();
+};
+
+
+export default {
+    createSubscription
+}
