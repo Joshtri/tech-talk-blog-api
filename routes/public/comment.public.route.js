b8d41544e16ec15919c68e5f379a2a66115377b4
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 13:44:03 +0800

    adding public route

---
+++ routes/public/comment.public.route.js
@@ -0,0 +1,14 @@
+// import * as commentController from '../../controllers/comment/comment.public.controller.js';
+import { commentPublicController } from '../../controllers/comment/index.js';
+import express from 'express';
+const commentPublicRoute = express.Router();
+
+
+
+commentPublicRoute.get('/comment/:id', commentPublicController.getCommentByIdPost);
+commentPublicRoute.post('/comment', commentPublicController.postComment);
+
+commentPublicRoute.get('/comment/count/:id', commentPublicController.countCommentById);
+
+
+export default commentPublicRoute;
