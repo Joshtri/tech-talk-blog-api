8bd3a7dd051e146275dc9bc8d5ec8ecac5c6f3a7
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 21:25:13 +0800

    adjust new file name

--- routes/private/post.cms.route.js
+++ routes/private/post.cms.route.js
@@ -1,22 +1,22 @@
 import express from "express";
-import { postCmsController } from "../../controllers/post/index.js"; // Mengimpor dari index.js
+import { postPrivateController } from "../../controllers/post/index.js"; // Mengimpor dari index.js
 import upload from "../../config/multerConfig.js";
 
 const postCmsRoute = express.Router();
 
 // Rute untuk CMS
-postCmsRoute.get("/post", postCmsController.getAllPosts); // Get all posts
-postCmsRoute.get("/post/:id", postCmsController.getPostById); // Get a specific post by ID
+postCmsRoute.get("/post", postPrivateController.getAllPosts); // Get all posts
+postCmsRoute.get("/post/:id", postPrivateController.getPostById); // Get a specific post by ID
 postCmsRoute.post(
   "/post",
   upload.single("coverImage"),
-  postCmsController.createPost // Create a new post
+  postPrivateController.createPost // Create a new post
 );
 postCmsRoute.put(
   "/post/:id",
   upload.single("coverImage"),
-  postCmsController.updatePost // Update an existing post
+  postPrivateController.updatePost // Update an existing post
 );
-postCmsRoute.delete("/post/:id", postCmsController.deletePost); // Delete a post by ID
+postCmsRoute.delete("/post/:id", postPrivateController.deletePost); // Delete a post by ID
 
 export default postCmsRoute;
