1fd419154f289a377399ec8f324e9efdf83d82e8
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 13:35:25 +0800

    merge private and public subscription service  in index.js

---
+++ services/subscription/index.js
@@ -0,0 +1,4 @@
+import subscriptionCmsService from "../subscription/subscription.cms.service.js";
+import subscriptionPublicService from "../subscription/subscription.public.service.js";
+
+export {  subscriptionPublicService , subscriptionCmsService};
