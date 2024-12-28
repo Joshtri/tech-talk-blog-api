3acdd9e2c47476a292c48ddb2cffae7f0828427f
Joshtri <stuffofyos1516@gmail.com>
2024-12-28 13:49:26 +0800

    rename and move files

--- repositories/subscription.repository.js
+++
@@ -1,7 +0,0 @@
-// repositories/subscription.repository.js
-import Subscription from "../models/subscription.model.js";
-
-export const createSubscription = async (subscriptionData) => {
-    const subscription = new Subscription(subscriptionData);
-    return await subscription.save();
-};
