e3484c87fb5d87442eaa4a2a449f278d18dcbea1
Joshtri <stuffofyos1516@gmail.com>
2024-12-31 12:32:06 +0800

    like public

--- routes/public/like.public.route.js
+++ routes/public/like.public.route.js
@@ -5,14 +5,13 @@ import { likePublicController } from "../../controllers/like/index.js";
 const likePublicRoute = express.Router();
 
 
-
 // Route untuk menyukai postingan
-likePublicRoute.post('/like', likePublicController.likePost);
+likePublicRoute.post("/like", likePublicController.likePost);
 
 // Route untuk membatalkan suka pada postingan
-likePublicRoute.post('/unlike', likePublicController.unlikePost);
+likePublicRoute.post("/unlike", likePublicController.unlikePost);
 
 // Route untuk mendapatkan jumlah like pada postingan
-likePublicRoute.get('/like/:postId', likePublicController.getLikeCountByPostId);
+likePublicRoute.get("/like/:postId", likePublicController.getLikeCountByPostId);
 
 export default likePublicRoute;
