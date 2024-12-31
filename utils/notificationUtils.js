394e01123b51debdd1a8985feda3fd92d64d11f4
Joshtri <stuffofyos1516@gmail.com>
2024-12-31 12:36:41 +0800

    adfajdf

---
+++ utils/notificationUtils.js
@@ -0,0 +1,9 @@
+export const sendEmail = (email, title, link) => {
+  console.log(`Email sent to ${email}: ${title} - ${link}`);
+  // Tambahkan logika untuk mengirim email
+};
+
+export const sendWhatsAppMessage = (phone, title, link) => {
+  console.log(`WhatsApp message sent to ${phone}: ${title} - ${link}`);
+  // Tambahkan logika untuk mengirim pesan WhatsApp
+};
