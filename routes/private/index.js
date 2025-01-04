d27660f3dcb94dc1720634caa012220624fff34a
Joshtri <stuffofyos1516@gmail.com>
2025-01-04 08:39:22 +0800

    updating index.js

--- routes/private/index.js
+++ routes/private/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import postCmsRoute from "./post.private.route.js"; // Import post CMS routes
 import subscriptionCmsRoute from "./subscription.private.route.js"; // Import subscription CMS routes
+import authRoute from "./auth.private.route.js";
+import maintenancePrivateRoute from "./maintenance.private.route.js";
 
 const privateRoutes = express.Router();
 
 // Gabungkan semua route private (CMS)
 privateRoutes.use("/", postCmsRoute); // Semua rute di post CMS akan menggunakan prefix /post
 privateRoutes.use("/", subscriptionCmsRoute); // Semua rute di subscription CMS akan menggunakan prefix /subscription
+privateRoutes.use("/auth", authRoute);
+privateRoutes.use("/", maintenancePrivateRoute);
+
 
 export default privateRoutes;
